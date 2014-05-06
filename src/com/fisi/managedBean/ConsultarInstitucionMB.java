a574500b3fee96e577059faf9b5258e9514d987b
Omar <Cuba.Omar13@gmail.com>
2014-05-06 03:08:02 -0500

    Correcion de listado de instituciones Falta modelo de base de datos DAO,DAOIMPL

--- src/com/fisi/managedBean/ConsultarInstitucionMB.java
+++ src/com/fisi/managedBean/ConsultarInstitucionMB.java
@@ -2,13 +2,16 @@ package com.fisi.managedBean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 import com.fisi.modelo.Institucion;
 @ManagedBean(name="consultarInstitucionMB")
-@SessionScoped
+@ApplicationScoped
 public class ConsultarInstitucionMB implements Serializable{
 	private int opcionBusqueda;
 	private ArrayList<Institucion> instituciones;
@@ -19,14 +22,22 @@ public class ConsultarInstitucionMB implements Serializable{
 	private void init() {
 		opcionBusqueda=1;
 		instituciones=new ArrayList<Institucion>();
-		Institucion ejemplo=new Institucion(1, "Von Newman", "Lima", "Lima","San Juan de Lurigancho");
+		//no funciono el ejemplo u.u
+		Institucion ejemplo=new Institucion(1, "guadalupe", "ugel 1","San Juan de Lurigancho");
 		instituciones.add(ejemplo);
+
 	}
 	public int getOpcionBusqueda() {
 		return opcionBusqueda;
 	}
 	public void setOpcionBusqueda(int opcionBusqueda) {
 		this.opcionBusqueda = opcionBusqueda;
+	}
+	public ArrayList<Institucion> getInstituciones() {
+		return instituciones;
+	}
+	public void setInstituciones(ArrayList<Institucion> instituciones) {
+		this.instituciones = instituciones;
 	}
 	
 }
